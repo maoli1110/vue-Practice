17ff5c5294592f0b9e62d17dae4e0843ccb161a6
maoli1110 <848631812@qq.com>
2017-03-02 18:54:45 +0800

    first commit

---
+++ src/components/sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="hello">
+        <div>sidebar++++</div>
+    </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+
+import Hello from 'components/Hello'
+
+export default [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    }
+]
