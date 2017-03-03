1113292f8b4521692b088635bc52e5496e805088
maoli1110 <848631812@qq.com>
2017-03-03 18:24:14 +0800

    vue-router&firstHtml

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -16,8 +16,10 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><router-link to="/enterprise">/enterprise</router-link></li>
     </ul>
   </div>
+   <!-- <router-view></router-view> -->
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,34 @@ import Hello from 'components/Hello'
 
 export default [
     {
-      path: '/',
+      path: '/', //顶层路由
       name: 'Hello',
-      component: Hello
+      component: Hello,
+      children:[
+      {
+      	path:'',
+      	redirect:'/enterprise'
+      },
+      {
+      	path:'/enterprise',
+      	component:Hello
+      },
+      {
+      	path:'/rootSetting',
+      	component:Hello
+      },
+      {
+      	path:'/projectData',
+      	component:Hello
+      },
+      {
+      	path:'/component',
+      	component:Hello
+      },
+      {
+      	path:'/login',
+      	component:Hello
+      }
+      ]
     }
 ]
